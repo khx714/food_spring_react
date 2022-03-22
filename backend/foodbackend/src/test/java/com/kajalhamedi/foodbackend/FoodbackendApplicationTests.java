3d326b9e6cd1ac8b1fb1eae1cdc4e110dbfe37cf
Kajal Hamedi <kajal.hamedi@gmail.com>
2022-03-22 13:22:44 +0200

    created project

---
+++ backend/foodbackend/src/test/java/com/kajalhamedi/foodbackend/FoodbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kajalhamedi.foodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
